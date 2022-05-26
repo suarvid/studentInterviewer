6c8eee06d578506d6e28ba79f7e4d0f0de26ecdf
Arvid Sundbom <arvid.sundbom98@gmail.com>
2022-05-26 19:29:54 +0200

    Finalized without text to speech

--- main.py
+++ main.py
@@ -4,23 +4,47 @@
 from question_tree import Question
 from topic import Topic
 
+def generate_weekday_topic():
+    q1 = Question("Which is the second day of the week?", ["tuesday"])
+    q2 = Question("Which is the last day of the week?", ["sunday", "some day", "some", "day", "someday"])
+    q3 = Question("Which day is between Sunday and Tuesday?", ["monday"])
+    q4 = Question("Which is the first day of the week?", ["monday"], [(q1, -2.0), (q2, -2.0), (q3, -2.0)])
+    q1.add_left_child(q2)
+    q1.add_right_child(q2)
+    q2.add_left_child(q3)
+    q2.add_right_child(q3)
+    q3.add_left_child(q4)
+    return Topic("Weekdays", [q1])
+
+def generate_alphabet_topic():
+    q1 = Question("How many letters does the english alphabet have?", ["26" ,"twenty six", "twenty", "six"])
+    q2 = Question("Which is the last letter of the english alphabet?", ["zed", "z", "zee", "sea", "c", "see", "sed", "set", "said", "sent"])
+    q3 = Question("Which is the 26th letter of the english alphabet?", ["zed", "z", "zee", "sea", "c", "see", "sed", "set", "said", "sent"], [(q1, -3.0), (q2, -3.0)])
+    q1.add_left_child(q2)
+    q1.add_right_child(q2)
+    q2.add_left_child(q3)
+    q2.add_right_child(q3)
+    return Topic("Alphabet", [q1])
+
+
 # Read in questions, answers and follow-up questions from file here
 def generate_topics():
-    # Vad är det som behöver skapas här?
-    # Några topics, t.ex börja med ett
-    # Vad behöver ett topic?
-    # Ett eller fler frågeträd
-    # Vad behöver ett frågeträd?
-    # Ett frågeträd är bara en massa Question-noder
-    # Så gör bara en massa question-noder för att testa
-    q1 = Question("What colour is the sky?", ["blue"])
-    q2 = Question("What day is between Monday and Wednesday?", ["tuesday"]) 
-    q1.add_left_child(q2)
-    t = Topic([q1])
-    return [t]
+    weekday = generate_weekday_topic()
+    alphabet = generate_alphabet_topic()
+    return [alphabet]
 
 
 if __name__ == "__main__":
     topics = generate_topics()
     for topic in topics:
         topic.evaluate_topic()
+    total_score = 0.0
+    threshold = 12.0
+    for topic in topics:
+        print(f"SCORE FOR TOPIC {topic.name}:")
+        topic_score = topic.get_topic_score()
+        print(topic_score)
+        total_score += topic_score
+    print(f"TOTAL SCORE: {total_score}")
+    print(f"THRESHOLD NEEDED TO PASS: {threshold}")
+
--- question_tree.py
+++ question_tree.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 import speech_recognition as sr
 import sys
+from gtts import gTTS
+import os
+from playsound import playsound
+import time
+
 
 class Question:
-    def __init__(self, data, correct_answer_tokens, weight=1.0):
+    def __init__(self,
+                 data,
+                 correct_answer_tokens,
+                 related_questions=[],
+                 weight=1.0):
         self.left = None
         self.right = None
         self.data = data
@@ -12,6 +21,8 @@ class Question:
         self.answer_score = 0.0
         self.correct_answer_tokens = correct_answer_tokens
         self.recognizer = sr.Recognizer()
+        self.related_questions = related_questions
+        self.answered_correctly = False
 
     def add_left_child(self, leftChild):
         self.left = leftChild
@@ -34,6 +45,7 @@ class Question:
         with sr.Microphone() as source:
             print("QUESTION:")
             print(self.data)
+            time.sleep(1.0)
             audio = self.recognizer.listen(source)
             try:
                 return self.recognizer.recognize_google(audio)
@@ -47,16 +59,31 @@ class Question:
         for token in response_tokens:
             if token in self.correct_answer_tokens:
                 print(f"GOOD RESPONSE: {response}")
+                print("That's correct!")
+                time.sleep(1.0)
+                self.answered_correctly = True
                 return 1
         print(f"BAD RESPONSE: {response}")
+        print("I'm sorry, that's not quite correct.")
+        time.sleep(1.0)
         return -1
 
     def get_tree_score(self):
         score = 0.0
         if self.left is not None:
-            get_tree_score(self.left)
+            score += self.left.get_tree_score()
         if self.right is not None:
-            get_tree_score(self.right)
+            score += self.right.get_tree_score()
         score += self.answer_score
+        score += self.calculate_inconsistency_penalties()
         return score
 
+    def calculate_inconsistency_penalties(self):
+        total_penalty = 0.0
+        if not self.answered_correctly:
+            for (question, penalty) in self.related_questions:
+                if not question.answered_correctly:
+                    return 0.0
+                total_penalty += penalty
+
+        return total_penalty
